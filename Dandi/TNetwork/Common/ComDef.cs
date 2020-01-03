0639404eb91fd4eefe35829e9f95de502eeef1a2
KyungHoon0126 <48943501+KyungHoon0126@users.noreply.github.com>
2020-01-03 15:31:39 +0900

    modify : TNetwork-Common-ComDef, Assets-Icon

--- Dandi/TNetwork/Common/ComDef.cs
+++ Dandi/TNetwork/Common/ComDef.cs
@@ -11,11 +11,7 @@ namespace TNetwork.Common
         public enum LOGIN_STATS { DISCONNECT, CONNECT };
 
         //소켓 서버 주소
-        public static string serverUrl { get; set; }//HOSTING
-        public const string LOCALSERVER1_URL = "http://10.80.161.236:3000"; //정한나
-        public const string LOCALSERVER2_URL = "http://10.80.161.145:3000"; //김시아
-        //public const string LOCALSERVER2_URL = "http://192.168.0.14:3000"; //김시아
-        public const string LOCALSERVER3_URL = "http://10.80.161.225:3000"; //박태형
+        public static string serverUrl { get; set; } //HOSTING
 
 		public const int TIME_OUT = 30000;
 
